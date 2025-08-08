99994afcc228e5ea57df8e04565ba5b597662506
Onur <onur.onel2230@gmail.com>
2025-08-08 15:23:04 -0400

    Initial commit

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import '@/styles/tailwind.css'
+import '@/styles/landing/landing.css'
+
+import { Hero } from '@/components/home/Hero'
+
+export default function HomePage() {
+  return (
+    <main>
+      <Hero />
+    </main>
+  )
+}
